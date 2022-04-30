84fd4c7fe6f2135c50df3ef450a790db268110fb
REDISKA88 <virusloki@ya.ru>
2022-04-30 18:26:35 +0300

    lets get it

---
+++ hrmph_shows/hrmph_shows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hrmph_shows
+//
+//  Created by Aura Antilochus on 4/30/22.
+//  Copyright © 2022 Roman CHAIKIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hrmph_shows/hrmph_showsTests/hrmph_showsTests.swift
@@ -0,0 +1,34 @@
+//
+//  hrmph_showsTests.swift
+//  hrmph_showsTests
+//
+//  Created by Aura Antilochus on 4/30/22.
+//  Copyright © 2022 Roman CHAIKIN. All rights reserved.
+//
+
+import XCTest
+@testable import hrmph_shows
+
+class hrmph_showsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
