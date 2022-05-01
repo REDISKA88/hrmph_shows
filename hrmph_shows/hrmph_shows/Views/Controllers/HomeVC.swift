792f12d6b7d70e438faeb07cc10362d850b32468
REDISKA88 <virusloki@ya.ru>
2022-05-01 17:25:34 +0300

    before change homeVc

--- hrmph_shows/hrmph_shows/Views/Controllers/HomeVC.swift
+++ hrmph_shows/hrmph_shows/Views/Controllers/HomeVC.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class HomeVC: MainTheme {
+class HomeVC: MainTheme, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let popularCellID = "popularCollectionViewCell"
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: popularCellID, for: indexPath)
+        return cell
+    }
+    
 
     @IBOutlet weak var popularCollectionView: UICollectionView!
     @IBOutlet weak var upcomingCollectionView: UICollectionView!
@@ -16,9 +27,15 @@ class HomeVC: MainTheme {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        popularCollectionView.delegate = self
+        popularCollectionView.dataSource = self
         
     }
+
+
+}
+
+class PopularCell: UICollectionViewCell {
     
-
-
+    
 }
