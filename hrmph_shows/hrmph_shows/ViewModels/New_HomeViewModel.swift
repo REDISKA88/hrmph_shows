dc46c54ab7252549ad69a902533d8740fa8bb4a1
REDISKA88 <virusloki@ya.ru>
2022-06-15 02:01:14 +0300

    subj

--- hrmph_shows/hrmph_shows/ViewModels/New_HomeViewModel.swift
+++ hrmph_shows/hrmph_shows/ViewModels/New_HomeViewModel.swift
@@ -1,15 +1,28 @@
-//
-//  New_HomeViewModel.swift
-//  hrmph_shows
-//
-//  Created by Aura Antilochus on 6/14/22.
-//  Copyright © 2022 Roman CHAIKIN. All rights reserved.
-//
-
+import RxCocoa
+import RxSwift
 import Foundation
 
 final class HomeViewModel {
     
+    private var networker = HomeNetworkManager.shared
+    private let _shows = BehaviorSubject<[Show]>(value: [])
     
+    init() {
+        self.fetchShows()
+    }
+    
+    func fetchShows() {
+        _shows.onNext([Show]())
+        networker.getShowData(id: "0") {[weak self] (result) in
+            switch result {
+            case .success(let returnedShows):
+                self?._shows.onNext(returnedShows)
+            case .failure(let error):
+                print(error.localizedDescription)
+                return
+            }
+        }
+    }
+
     
 }
